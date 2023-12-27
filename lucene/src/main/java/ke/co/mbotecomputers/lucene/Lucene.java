0b1cb83eebb034f3d1f2724d6debdebde64a4f8a
chege <chege@chege>
2023-12-27 18:16:04 +0300

    init repo

---
+++ lucene/src/main/java/ke/co/mbotecomputers/lucene/Lucene.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ke.co.mbotecomputers.lucene;
+
+/**
+ *
+ * @author chege
+ */
+public class Lucene {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

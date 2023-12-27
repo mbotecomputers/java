f848190c3031ea4e108e5fc27b64698d762c7aae
chege <chege@chege>
2023-12-27 17:34:46 +0300

    init repo

---
+++ hello/src/main/java/ke/co/mbotecomputers/hello/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ke.co.mbotecomputers.hello;
+
+/**
+ *
+ * @author chege
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
